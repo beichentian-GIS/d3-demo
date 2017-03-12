cfe015368467ab11ac45a771e1f1608ab22df3d5
Beichen Tian <btian8@wisc.edu>
2017-03-12 15:56:27 -0500

    Module 7 assignment completed
    Completed the tasks listed in the end of module 7

--- js/main.js
+++ js/main.js
@@ -1,37 +1,155 @@
-var container=d3.select("body")
-	.append("svg")
-	.datum(500)
-	.attr("width",function(d){
-		console.log(d);
-		return d+'px';
-	})
-	.attr("height","500px")
-	.attr("class","container")
+//SVG dimension variables
+var w=900, h=500;
 
-var dataArray=[25,45,65,85];
+//execute script when window is loaded
+window.onload=function(){
+	var container=d3.select("body") //get the <body> element from the DOM 
+		.append("svg") //put a new svg in the body
+		.attr("width",w) //assign the width
+		.attr("height",h) //assign the height
+		.attr("class","container") //always assign the class (as the block name) for styling and future selection
+		.style("background-color","rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
 	
-var x=d3.scaleLinear()
-	.domain([0,50])
-	.range([0,480]);
-	
-	
-d3.json("data/MegaCitiesGeo.geojson",function(data){
-	console.log(data);
-	var h=5;
-	var textElements=container.selectAll(".textElement");
-	.data(dataArray)
-	.enter(); //create a loop
-	.append("text")
-	.attr("class","textElement")
-	.attr("x",0)
-	.attr("y",function(d){
-		return x(d.properties.Pop_2015);
-	})
-	.text(function(d){
-		return d.properties.City;
-	});
-})
-	
+	//innerRect block
+	var innerRect=container.append("rect") //put a new rect in the svg
+		.datum(400) //a single value is a datum
+		.attr("width",function(d){ //rectangle width
+			return d*2; //400*2=800
+		}) 
+		.attr("height",function(d){ //rectangle height
+			return d; //400
+		})
+		.attr("class","innerRect") //class name
+		.attr("x",50) //position from left on the x (horizontal) axis
+		.attr("y",50) //position from top on the y (vertical) axis
+		.style("fill","#FFFFFF"); //fill color
 
-	
-console.log(textElements);
+		var cityPop=[
+			{
+				city:'Minneapolis',
+				population:407207
+			},
+			{
+				city:'St Paul',
+				population:297640
+			},
+			{
+				city:'Rochester',
+				population:111402
+			},
+			{
+				city:'Woodbury',
+				population:66807
+			}
+		];
+		
+		var x=d3.scaleLinear() //create the scale
+			.range([90,810]) //output min and max
+			.domain([0,3.3]) //input min and max
+		console.log(x)
+		
+		//find the minimum value of the array
+		var minPop=d3.min(cityPop,function(d){
+			return d.population;
+		});
+		
+		//find the maximum value of the array
+		var maxPop=d3.max(cityPop,function(d){
+			return d.population;
+		});
+		
+		//scale for circles center y coordinate
+		var y=d3.scaleLinear()
+			.range([450,50]) //was 440, 95
+			.domain([0,500000]); //was minPop, maxPop
+		
+		//color scale generator
+		var color=d3.scaleLinear()
+			.range([
+				"#FDBE85",
+				"#D94701"
+			])
+			.domain([
+				minPop,
+				maxPop
+			]);
+		
+		var circles=container.selectAll(".circles")
+			.data(cityPop) //here we feed in an array
+			.enter() 
+			.append("circle") //add a circle for each datum
+			.attr("class","circles") //apply a class name to all circles
+			.attr("id",function(d){ //circle radius
+				return d.city;
+			})
+			.attr("r",function(d){
+			//calculate the radius based on population value as circle area
+				var area=d.population*0.01;
+				return Math.sqrt(area/Math.PI);
+			})
+			.attr("cx",function(d,i){
+				//use the scale generator with the index to place each circle horizontally
+				return x(i);
+			})
+			.attr("cy",function(d){
+				return y(d.population);
+			})
+			.style("fill",function(d,i){ //add a fill based on the color scale generator
+				return color(d.population);
+			})
+			.style("stroke","#000"); //black circle stroke
+			
+			//create y axis generator
+			var yAxis=d3.axisLeft(y);
+			
+			//create axis g element and add axis
+			var axis=container.append("g")
+				.attr("class","axis")
+				.attr("transform","translate(50,0)")
+				.call(yAxis);
+				
+			//create a text element and add the title
+			var title=container.append("text")
+				.attr("class","title")
+				.attr("text-anchor","middle")
+				.attr("x",450)
+				.attr("y",30)
+				.text("City Populations");
+			
+			//create circle labels
+			var labels=container.selectAll(".labels")
+				.data(cityPop)
+				.enter()
+				.append("text")
+				.attr("class","labels")
+				.attr("text-anchor","left")
+				.attr("y",function(d){
+					//vertical position centered on each circle
+					return y(d.population)+2;
+				});
+				
+				//first line of label
+				var nameLine=labels.append("tspan")
+					.attr("class","nameLine")
+					.attr("x",function(d,i){
+						//horizontal position to the right of each circle
+						return x(i)+Math.sqrt(d.population*0.01/Math.PI)+5;
+					})
+					.text(function(d){
+						return d.city;
+					});
+				
+				var format=d3.format(",");
+				
+				//second line of label
+				var popLine=labels.append("tspan")
+					.attr("class","popLine")
+					.attr("x",function(d,i){
+						//horizontal position to the right of each circle
+						return x(i)+Math.sqrt(d.population*0.01/Math.PI)+5;
+					})
+					.attr("dy","15") //vertical offset
+					.text(function(d){
+						return "Pop. "+format(d.population); //use format generator to format numbers
+					});
+};
